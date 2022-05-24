1fab7550b9b2c151fb0d0956abe2289b36fe215c
bienzobasjulian <bienzoj@gmail.com>
2022-05-24 21:52:21 +0200

    start login with firebase (With errors in ngIf)

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +8,7 @@ import { Component, ElementRef, HostListener, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor(private eRef: ElementRef) { }
+  constructor(private eRef: ElementRef, private authService : AuthService) { }
 
   ngOnInit(): void {
   }
@@ -15,6 +16,24 @@ export class HeaderComponent implements OnInit {
   menuActivado : boolean = false;
   menuSorteosActivado : boolean = false;
   menuDecisionesActivado : boolean = false;
+
+  usuarioLogueado = this.authService.getUserLogged();
+  
+  
+  
+  
+  printUsuarioLog(){
+    
+    this.authService.getUserLogged().subscribe(res => {
+      console.log(res?.email);
+    
+    });
+  }
+
+  logOut(){
+    this.authService.logOut();
+    
+  }
 
   toggleMenu (){
     this.menuActivado = !this.menuActivado;
